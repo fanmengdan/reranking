d9b0391c58a1a31ab2cb3a08f79ebb5e06533f5c
Sandesh C <sandeshc13@gmail.com>
2017-04-08 16:02:42 +0530

    modified doc2vec.sh to run multiple dimen PV train/test

--- doc2vec.sh
+++ doc2vec.sh
@@ -1,23 +1,29 @@
 mode="dbow"
 meph=10
-neph="5,10"
-ephs=(5 10)
+neph="1,3,5,10"
+ephs=(1 3 5 10)
 wndw=(10 15 20)
-dimen=200
+dimens=(100 200)
 
-for w in "${wndw[@]}"
+for dimen in "${dimens[@]}"
 do
-    printf '######################## python train.py %3d %3d %s %s %3d ######################\n' "$w" "$meph" "$neph" "$mode" "$dimen"
-    python train.py $w $meph $neph $mode $dimen
-    printf '\n\n\n#######################################################################\n\n\n'
+    for w in "${wndw[@]}"
+    do
+        printf '######################## python train.py %3d %3d %s %s %3d ######################\n' "$w" "$meph" "$neph" "$mode" "$dimen"
+        python train.py $w $meph $neph $mode $dimen
+        printf '\n\n\n#######################################################################\n\n\n'
+    done
 done
 
-for w in "${wndw[@]}"
+for dimen in "${dimens[@]}"
 do
-    for n in "${ephs[@]}"
+    for w in "${wndw[@]}"
     do
-        printf '######################## python test.py %3d %3d %s %3d #######################\n' "$w" "$n" "$mode" "$dimen"
-        python test.py $w $n $mode $dimen
-        printf '\n\n\n#######################################################################\n\n\n'
+        for n in "${ephs[@]}"
+        do
+            printf '######################## python test.py %3d %3d %s %3d #######################\n' "$w" "$n" "$mode" "$dimen"
+            python test.py $w $n $mode $dimen
+            printf '\n\n\n#######################################################################\n\n\n'
+        done
     done
 done
